5cf3bc4607ff7dea895f9c7215d8f11d5b1a74ed
SimonCodesf <simon.beel@student.luca-arts.be>
2025-09-13 17:15:58 +0200

    Add explicit homepage route to fix 404 error
    - Added Route::statamic('/', 'home') in web.php for explicit root routing
    - This bypasses collection mounting issues and directly routes / to home template
    - Should resolve the persistent 404 error on the homepage

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\LiveSearchController;
 
+// Explicit home route
+Route::statamic('/', 'home', [
+   'title' => 'Thila Coloma',
+   'template' => 'home',
+   'layout' => 'layouts/app'
+]);
+
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
